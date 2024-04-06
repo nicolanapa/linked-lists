06334ac829bd565a06a91bd20a56b5ed5201291d
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-04-06 18:15:50 +0200

    Rewrote the main function, so now it should atleast work once in the prepend() function

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 class Node {
-	constructor(value, nextNode) {
+	constructor(value /*, nextNode*/) {
 		this.value = value;
-		this.nextNode = nextNode;
+		this.nextNode = null;
+
+		console.log("Node:", this.value, this.nextNode);
 	}
 }
 
-class LinkedList {
-	/*constructor(value, nextNode) {
+/*constructor(value, nextNode) {
 		if (value !== null) {
 			if (nextNode === null) {
 				this.value = value;
@@ -34,13 +35,11 @@ class LinkedList {
 		new Node(this.value, this.nextNode);
 	}*/
 
-	// End of list
-	append(value) {
+// End of list
+/*append(value) {
 		console.log("Appending...");
 
-		if (this.nextNode === null || this.nextNode === undefined) {
-			return new Node(value, null);
-		}
+		//return new Node(value);
 
 		let temp = this.nextNode;
 
@@ -54,55 +53,59 @@ class LinkedList {
 			temp = temp.nextNode;
 			return new Node(value, temp);
 		}
+	}*/
+
+// Start of list
+function prepend(head, value) {
+	console.log("Prepending...");
+
+	let newNode = new Node(value);
+	newNode.next = head[0];
+	head[0] = newNode;
+}
+
+function size() {}
+
+function head(list) {
+	console.log("Returning head...");
+
+	return console.log(list.value);
+}
+
+function tail() {}
+
+function at(index) {}
+
+//set pop() {}
+
+function contains(value) {}
+
+function find(value) {}
+
+function toString(list) {
+	console.log("Converting toString...");
+
+	let string = "";
+	let temp = list;
+	while (temp !== null) {
+		string += "( " + temp.value + " ) -> ";
+		temp = temp.nextNode;
 	}
-
-	// Start of list
-	prepend(value) {
-        console.log("Prepending...");
-        
-        new Node(value, null);
-    }
-
-	get size() {}
-
-	get head() {}
-
-	get tail() {}
-
-	at(index) {}
-
-	//set pop() {}
-
-	contains(value) {}
-
-	find(value) {}
-
-	get toString() {
-		console.log("Converting toString...");
-
-		let string = "";
-		let temp = this;
-		while (temp != null) {
-			string += "( " + temp.value + " ) -> ";
-            temp = temp.nextNode;
-		}
-		//temp = temp.value;
-		if (temp === null || temp === undefined) {
+	//temp = temp.value;
+	/*if (temp === null || temp === undefined) {
 			string += null;
 		} else {
 			string += temp + " (This shouldn't happen!)";
-		}
-
-		console.log("toString:", string);
-	}
+		}*/
+	string += null;
+	console.log(string);
 }
 
-let prova0 = new LinkedList();
-prova0.append("prova2");
-prova0.prepend("prova1");
-//prova0.Node.append = "prova3";
-//console.log(prova0.nextNode);
-//console.log(prova0.append("ciao"));
-//console.log(new Node("apple", "frutta1"));
-//console.log(new Node(null, null));
-prova0.toString;
+let prova0 = [null];
+//prova0.append("prova2");
+prepend(prova0, "prova2");
+prepend(prova0, "prova1");
+prepend(prova0, "prova0");
+
+head(prova0[0]);
+toString(prova0[0]);
